3041f3968904b8c7b0f3fbd4d0a237b49847c53e
voducanhkhoi07-crypto <voducanhkhoi.07@gmail.com>
2025-11-18 16:54:48 +0700

    toan hoc

---
+++ Biểu thức toán học.py
@@ -0,0 +1,5 @@
+def bthuc(x):
+    return x
+x=int(input('Nhập x'))
+A = x*x+3*x-5
+print(A)
---
+++ tong-tich-hai-so.py
@@ -0,0 +1,7 @@
+#Tính tổng và tích của hai số
+def tong_tich(a,b):
+    return a+b,a*b
+a=int(input('nhập số a'))
+b=int(input('nhập số b'))
+print('Tổng hai số là',a+b)
+print('Tích hai số là',a*b)
