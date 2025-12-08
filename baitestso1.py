90eb37188cffa529acc38019f2ca6c869eb3e301
voducanhkhoi07-crypto <voducanhkhoi.07@gmail.com>
2025-12-08 13:47:23 +0700

    Create baitestso1.py

---
+++ baitestso1.py
@@ -0,0 +1,10 @@
+# Nhập 3 số
+a = float(input("Nhập số a: "))
+b = float(input("Nhập số b: "))
+c = float(input("Nhập số c: "))
+
+# Tính trung bình
+trung_binh = (a + b + c) / 3
+
+# In kết quả
+print("Trung bình của 3 số là:", trung_binh)
